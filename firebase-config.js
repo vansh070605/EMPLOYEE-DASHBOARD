6732b26b852c1d6521aba13838e4afa60a9631db
Vansh Agrawal <vansh070605@gmail.com>
2025-07-17 11:20:24 +0530

    Git ignore

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCJpuPZ-tdB3JiSUpihmrMANqFu1bOAtJ8",
+  authDomain: "kanban-employee-dashboard.firebaseapp.com",
+  projectId: "kanban-employee-dashboard",
+  storageBucket: "kanban-employee-dashboard.appspot.com",
+  messagingSenderId: "1041175504709",
+  appId: "1:1041175504709:web:8c5d72438ae0e6597f402c"
 };
